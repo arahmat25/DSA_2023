fc614582fb369f179f5cb0e0a8e12797f8dc9e83
Max Krusz <98424311+maxKrusz@users.noreply.github.com>
2023-04-23 21:56:51 -0400

    Implement BTreeNode Class

--- B_Tree.cpp
+++ B_Tree.cpp
@@ -1,12 +1,29 @@
-#include "B_Tree.h"
 #include <iostream>
 #include <vector>
+#include <string>
 #include <fstream>
 #include <sstream>
+#include <set>
 
 using namespace std;
 
-BTreeNode::BTreeNode(int degree, bool leaf){
+// B-Tree node class 
 
-}
+class BTreeNode{
+
+    int degree; // degree of tree, used to calculate lower and upper bounds of keys/children
+    bool leaf; //true when node is leaf, else false
+    int numKeys; // number of keys in node
+    int *keys; // vector of keys
+    BTreeNode **child_ptr;  // vector of pointers to children
+
+public:
+    BTreeNode(int degree, bool leaf); // node constructor
+    void insertNonFull(int key); //  inserts a key into a node that is not filled
+    void printTree(); // outputs the path
+    void printDot(BTreeNode *temp,string fname); // outputs dot visualization
+    void splitChild(int i, BTreeNode *y); // split a child node
+    BTreeNode *searchTree(int key); // searches for a key in a subtree
+    friend class BTree;
+};
 
