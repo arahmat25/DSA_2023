35494d31991729acf45cd1b530fc32c0e12decb7
Max Krusz <98424311+maxKrusz@users.noreply.github.com>
2023-04-23 22:00:38 -0400

    Create the start of the main function

--- main.cpp
+++ main.cpp
@@ -5,10 +5,23 @@
 #include "B_Tree.h"
 #include "B_Tree.cpp"
 
-int main(int argc, char*argv[]){
+int main(int argc, char* argv[]) {
 
-  
+    // Take in the filename a command line argument and assign it to a string
+    string filename = argv[1];
 
+    // Initialize degree integer and ask the user what degree they would like to set for the B-Tree
+    int degree;
+    cout << "What would you like the degree of this B-Tree to be? Input integer here:";
+    cin >> degree;
+
+    // Construct B-Tree
+    BTree tree(degree);
+
+    // Call read file function
+    read_file(filename, &tree);
+
+    return 0;
 
 }
 
