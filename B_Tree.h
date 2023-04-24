3b772bb60f658ad8df0b06bcd5b18dc3e331776e
Max Krusz <98424311+maxKrusz@users.noreply.github.com>
2023-04-23 22:03:32 -0400

    Change BTreeNode class

--- B_Tree.h
+++ B_Tree.h
@@ -10,25 +10,21 @@ using namespace std;
 // B-Tree node class 
 
 class BTreeNode{
+
     int degree; // degree of tree, used to calculate lower and upper bounds of keys/children
     bool leaf; //true when node is leaf, else false
     int numKeys; // number of keys in node
-    vector<int> *keys; // vector of keys
-    vector<BTreeNode> **child_ptr;  // vector of pointers to children
+    int *keys; // vector of keys
+    BTreeNode **child_ptr;  // vector of pointers to children
 
 public:
-
     BTreeNode(int degree, bool leaf); // node constructor
-
     void insertNonFull(int key); //  inserts a key into a node that is not filled
-    void navigate(); // outputs the path the
+    void printTree(); // outputs the path
+    void printDot(BTreeNode *temp,string fname); // outputs dot visualization
     void splitChild(int i, BTreeNode *y); // split a child node
-
-    BTreeNode *search(int key); // searches for a key in a subtree
-
+    BTreeNode *searchTree(int key); // searches for a key in a subtree
     friend class BTree;
-
-
 };
 
 class BTree{
