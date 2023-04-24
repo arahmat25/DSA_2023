c8fe9c7e552a1464f6887d28893c530e65032ff4
Max Krusz <98424311+maxKrusz@users.noreply.github.com>
2023-04-24 16:07:24 -0400

    Added count to search

--- B_Tree.h
+++ B_Tree.h
@@ -23,7 +23,7 @@ public:
     void printTree(); // outputs the path
     void printDot(BTreeNode *temp,string fname); // outputs dot visualization
     void splitChild(int i, BTreeNode *y); // split a child node
-    BTreeNode *searchTree(int key); // searches for a key in a subtree
+    BTreeNode *searchTree(int key, int count); // searches for a key in a subtree, count tracks how many nodes have been searched.
     friend class BTree;
 };
 
@@ -31,12 +31,11 @@ class BTree{
 
     int degree;  // degree of tree, used to calculate lower and upper bounds of keys/children
     BTreeNode *root; // root pointer
-
 public:
     explicit BTree(int _degree);// tree constructor
     void printTree(){root->printTree();} // outputs path
     void printDot();
     void insert(int key); // The main function that inserts a new key in this B-Tree
     // function to search a key in this tree
-    BTreeNode* search(int key){return root->searchTree(key);}
+    BTreeNode* search(int key, int count){return root->searchTree(key, count);} // searches for a key, count tracks how many nodes have been searched.
 };
